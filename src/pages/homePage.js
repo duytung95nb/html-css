52e1fc0b466f21a94657ff47444dc3a290a739c8
duytung95nb <duytung95nb@gmail.com>
2017-08-23 09:14:37 +0700

    - Change position between property and method

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,7 +1,7 @@
 
 function HomePage() {
-    this.render = render;
     this.templateUrl = "src/pages/homePage.html";
+    this.render = render;
     return this;
     function render(selector = "body") {
         window.uiHelper.renderHtmlFromTemplateUrl(selector, this.templateUrl).catch(function (error){
